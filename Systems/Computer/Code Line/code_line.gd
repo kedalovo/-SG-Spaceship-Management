45a2c4ba6b4370c598afa547c7e6ecfe0a29d7e8
kedalovo <69755184+kedalovo@users.noreply.github.com>
2024-11-17 13:33:19 +0400

    Finished Computer. With that, all systems are done to the point of moving on to other things. Will return to systems later to create proper interaction with the game.

--- Systems/Computer/Code Line/code_line.gd
+++ Systems/Computer/Code Line/code_line.gd
@@ -3,15 +3,25 @@ extends Node2D
 
 class_name CodeLine
 
+signal installed_correct_line(line: CodeLine, slot: CodeLine)
+
 
 @onready var label: Label = $Label
 @onready var own_body: CharacterBody2D = $Body
 @onready var area: Area2D = $Area
 @onready var button: Button = $Button
 
+var hovered_over: CodeLine
+var installed_on: CodeLine
+
+var length: int = 0
+var slot_length: int = 0
+
 var is_on_right: bool = false
 var is_held: bool = false
 var is_slot: bool = false
+var is_installed: bool = false
+var is_correct: bool = false
 
 
 func _physics_process(_delta: float) -> void:
@@ -27,6 +37,7 @@ func set_type(is_right: bool) -> void:
 		button = $Button
 	is_on_right = is_right
 	if is_right:
+		modulate = Color("fffc4a")
 		area.monitoring = false
 		area.hide()
 		own_body.show()
@@ -34,6 +45,7 @@ func set_type(is_right: bool) -> void:
 		button.show()
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 	else:
+		modulate = Color("ffffff")
 		area.monitoring = false
 		area.hide()
 		own_body.hide()
@@ -42,12 +54,14 @@ func set_type(is_right: bool) -> void:
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
 
 
-func set_slot() -> void:
+func set_slot(new_length: int) -> void:
 	if label == null or own_body == null or area == null or button == null:
 		label = $Label
 		own_body = $Body
 		area = $Area
 		button = $Button
+	slot_length = new_length
+	modulate = Color("ffffff64")
 	area.monitoring = true
 	area.show()
 	own_body.hide()
@@ -57,24 +71,59 @@ func set_slot() -> void:
 
 func set_text(text: String) -> void:
 	label.text = text
+	length = text.length()
+
+
+func hide_label() -> void:
+	label.hide()
+
+
+func show_label() -> void:
+	label.show()
+
+
+func check_if_correct(installed_length: int) -> bool:
+	if installed_length == slot_length:
+		is_correct = true
+	else:
+		is_correct = false
+	return is_correct
 
 
 func _on_button_button_down() -> void:
 	if is_on_right:
+		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 		is_held = true
+		if is_installed:
+			installed_on.show_label()
+			installed_on.is_correct = false
 
 
 func _on_button_button_up() -> void:
 	if is_held:
 		is_held = false
+	if hovered_over != null:
+		global_position = hovered_over.global_position
+		installed_on = hovered_over
+		installed_on.hide_label()
+		if installed_on.check_if_correct(length):
+			installed_on.installed_correct_line.emit(self, installed_on)
+		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+		is_installed = true
+	else:
 		position = Vector2.ZERO
 
 
 func _on_area_body_entered(body: Node2D) -> void:
 	var parent := body.get_parent()
 	if parent is CodeLine:
-		print("Code line entered!")
+		parent.hovered_over = self
 
 
 func _on_area_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	var parent := body.get_parent()
+	if parent is CodeLine:
+		parent.hovered_over = null
+		if parent.is_held and parent.is_installed:
+			parent.is_installed = false
+			parent.installed_on = null
--- Systems/Computer/computer_system.gd
+++ Systems/Computer/computer_system.gd
@@ -7,24 +7,111 @@ const CODE_LINE = preload("res://Systems/Computer/Code Line/code_line.tscn")
 @onready var puzzle_v_box: VBoxContainer = $UI/Margin/HBox/PuzzleVBox
 @onready var pieces_v_box: VBoxContainer = $UI/Margin/HBox/PiecesVBox
 
-var busy_puzzle_slots: Array[bool] = [false, false, false, false, false]
-var busy_pieces_slots: Array[bool] = [false, false, false, false, false]
+var busy_puzzle_slots: Array[bool] = [false, false, false, false, false, false]
+var busy_pieces_slots: Array[bool] = [false, false, false, false, false, false]
+
+var slots: Array[CodeLine] = []
 
 
-func _ready() -> void:
-	for i in 2:
-		add_code_line(false, game_manager.get_random_text(5))
-	add_empty_slot()
-	for i in 2:
-		add_code_line(false, game_manager.get_random_text(5))
-	for i in 2:
-		add_code_line(true, game_manager.get_random_text(6))
+func _damage(_strength: int, _type: game_manager.damage_types) -> void:
+	if _type == game_manager.damage_types.ELECTRICITY:
+		create_random_pattern(_strength)
 
 
-func add_empty_slot() -> void:
+func create_random_pattern(size: int) -> void:
+	size = clampi(size, 3, 6)
+	match randi()%4:
+		0:
+			print("Generating flat scenario...")
+			var length: int = randi()%4 + 4
+			var array: Array[int] = []
+			for i in size:
+				array.append(1)
+			array[0] = 0
+			array.shuffle()
+			var right := array.duplicate()
+			right.shuffle()
+			for i in array:
+				if i == 0:
+					add_empty_slot(length)
+				else:
+					add_code_line(false, game_manager.get_random_text(length))
+			for i in right:
+				if i == 0:
+					add_code_line(true, game_manager.get_random_text(length))
+				else:
+					add_code_line(true, game_manager.get_random_text(length - ([-2, -1, 1, 2].pick_random())))
+		1:
+			print("Generating mountain scenario...")
+			var length: int = randi()%3 + 6
+			var array: Array[int] = []
+			for i in size:
+				array.append(1)
+			array[0] = 0
+			array.shuffle()
+			var right := array.duplicate()
+			right.shuffle()
+			var idx: int = array.find(0)
+			for i in array.size():
+				if array[i] == 1:
+					add_code_line(false, game_manager.get_random_text(length - absi(idx - i)))
+				else:
+					add_empty_slot(length - absi(idx - i))
+			for i in right:
+				if i == 0:
+					add_code_line(true, game_manager.get_random_text(length))
+				else:
+					add_code_line(true, game_manager.get_random_text(length - ([-2, -1, 1, 2].pick_random())))
+		2:
+			print("Generating dip scenario...")
+			var length: int = randi()%4 + 2
+			var array: Array[int] = []
+			for i in size:
+				array.append(1)
+			array[0] = 0
+			array.shuffle()
+			var right := array.duplicate()
+			right.shuffle()
+			var idx: int = array.find(0)
+			for i in array.size():
+				if array[i] == 1:
+					add_code_line(false, game_manager.get_random_text(length + absi(idx - i)))
+				else:
+					add_empty_slot(length + absi(idx - i))
+			for i in right:
+				if i == 0:
+					add_code_line(true, game_manager.get_random_text(length))
+				else:
+					add_code_line(true, game_manager.get_random_text(length - ([-2, -1, 1, 2].pick_random())))
+		3:
+			print("Generating rise scenario...")
+			var length: int = randi()%3 + 6
+			var array: Array[int] = []
+			for i in size:
+				array.append(1)
+			array[0] = 0
+			array.shuffle()
+			var right := array.duplicate()
+			right.shuffle()
+			var idx: int = array.find(0)
+			for i in array.size():
+				if array[i] == 1:
+					add_code_line(false, game_manager.get_random_text(length - (idx - i)))
+				else:
+					add_empty_slot(length - (idx - i))
+			for i in right:
+				if i == 0:
+					add_code_line(true, game_manager.get_random_text(length))
+				else:
+					add_code_line(true, game_manager.get_random_text(length - ([-2, -1, 1, 2].pick_random())))
+
+
+func add_empty_slot(new_length: int) -> void:
 	var code_line: CodeLine = CODE_LINE.instantiate()
-	code_line.set_slot()
-	code_line.set_text("")
+	code_line.set_slot(new_length)
+	code_line.set_text("missing...")
+	code_line.installed_correct_line.connect(_on_installed_correct_line)
+	slots.append(code_line)
 	for slot_idx in busy_puzzle_slots.size():
 		if !busy_puzzle_slots[slot_idx]:
 			busy_puzzle_slots[slot_idx] = true
@@ -35,6 +122,7 @@ func add_empty_slot() -> void:
 
 
 func add_code_line(is_right: bool, text: String) -> void:
+	var added: bool = false
 	var code_line: CodeLine = CODE_LINE.instantiate()
 	code_line.set_type(is_right)
 	code_line.set_text(text)
@@ -45,6 +133,7 @@ func add_code_line(is_right: bool, text: String) -> void:
 				var pos: Control = pieces_v_box.get_child(slot_idx)
 				pos.add_child(code_line)
 				pos.show()
+				added = true
 				break
 	else:
 		for slot_idx in busy_puzzle_slots.size():
@@ -53,4 +142,12 @@ func add_code_line(is_right: bool, text: String) -> void:
 				var pos: Control = puzzle_v_box.get_child(slot_idx)
 				pos.add_child(code_line)
 				pos.show()
+				added = true
 				break
+	if !added:
+		push_warning("Couldn't add code_line, all slots are busy. Right: ", code_line.is_on_right)
+		code_line.queue_free()
+
+
+func _on_installed_correct_line(line: CodeLine, slot: CodeLine) -> void:
+	print("Installed correctly!")
